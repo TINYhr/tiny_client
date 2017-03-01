60e2022a841f3dcaefb415f2361d9b83ef973c92
Jonathan <jonathan@tinypulse.com>
2017-03-01 19:00:51 +0700

    Remove token and add default sample headers

--- lib/tiny_client/configuration.rb
+++ lib/tiny_client/configuration.rb
@@ -9,8 +9,8 @@ module TinyClient
     def initialize
       @url = 'http://localhost:3000/api/1.0'
       @headers = {
-        'Authorization' => 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwcC50aW55cHVsc2UuY29tIiwiaWF0IjoxNDg3NjUxNzY5LCJqdGkiOiJhNDA1YjIxMTdmNGQ3ODc0ODAxM2E0YWI2ZTU4YmMzYiIsInN1YiI6IlRJTllwdWxzZSBBUEkgYWNjZXNzIFRva2VuIiwiYXVkIjoiVElOWXB1bHNlIn0.hstm7u6cIvSzebRC6RkIDIUjPHQPKMHSKMggibtUF6s',
         'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
         'User-Agent' => 'TINYpulse client 1.0'
       }
     end
