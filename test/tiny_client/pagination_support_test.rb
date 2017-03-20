8ca80db855644ff36ed53bc6630c53b688549c19
Jonathan <jonathan@tinypulse.com>
2017-03-20 12:32:11 +0700

    Batch is now a method

--- test/tiny_client/pagination_support_test.rb
+++ test/tiny_client/pagination_support_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 describe TinyClient::PaginationSupport do
+  def batch(range)
+    range.each_with_object([]) { |i, a| a << { id: i } }
+  end
+
   MyResource = Class.new(TinyClient::Resource)
-  let('batch') { ->(range) { range.each_with_object([]) { |i, a| a << { id: i } } } }
 
   it { MyResource.must_respond_to :get_all }
   it { MyResource.must_respond_to :index_all }
@@ -20,7 +23,7 @@ describe TinyClient::PaginationSupport do
 
     it 'make one query when the first batch contains less than limit' do
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 0 })
-                                                   .to_return(body: batch.call(1..9).to_json)
+                                                   .to_return(body: batch(1..9).to_json)
       first = MyResource.get_all.first
       first.must_be_instance_of MyResource
       first.id.must_equal 1
@@ -29,7 +32,7 @@ describe TinyClient::PaginationSupport do
 
     it 'make two query when the first batch contains exactly limit' do
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 0 })
-                                                   .to_return(body: batch.call(1..10).to_json)
+                                                   .to_return(body: batch(1..10).to_json)
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 10 })
                                                    .to_return(body: [].to_json)
       MyResource.get_all.count.must_equal 10
@@ -39,11 +42,11 @@ describe TinyClient::PaginationSupport do
 
     it 'make three query when there is 28 elements and the limit is 10' do
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 0 })
-                                                   .to_return(body: batch.call(1..10).to_json)
+                                                   .to_return(body: batch(1..10).to_json)
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 10 })
-                                                   .to_return(body: batch.call(11..20).to_json)
+                                                   .to_return(body: batch(11..20).to_json)
       stub_request(:get, 'http://acme.org/my.json').with(query: { limit: 10, offset: 20 })
-                                                   .to_return(body: batch.call(21..28).to_json)
+                                                   .to_return(body: batch(21..28).to_json)
 
       MyResource.get_all.count.must_equal 28
       assert_requested :get, 'http://acme.org/my.json?limit=10&offset=0',  times: 1
