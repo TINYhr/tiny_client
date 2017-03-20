93750fa53291c066264e2c44aa6b17089c6170a1
Jonathan <jonathan@tinypulse.com>
2017-03-20 12:08:43 +0700

    Headers are an empty hash by default

--- lib/tiny_client/configuration.rb
+++ lib/tiny_client/configuration.rb
@@ -3,11 +3,10 @@ module TinyClient
   # Subclass and override {#initialize} to implement a client confiuration.
   # @abstract
   # @attr_reader [String] url the api root url (i.e: http://localhost/api/1.0)
-  # @attr_reader [Hash] headers default headers you want to pass along every request
   # @attr_reader [Integer] limit default limit used as a query param
   class Configuration
     include Singleton
-    attr_reader :url, :headers, :limit
+    attr_reader :url, :limit
 
     # You need to initialize the api {#url}, default {#headers}, and default limit.
     def initialize
@@ -17,6 +16,11 @@ module TinyClient
     # @return [Integer] request connection timeout in seconds
     def connect_timeout
       @connect_timeout ||= 30
+    end
+
+    # @return [Hash] headers default headers you want to pass along every request
+    def headers
+      @headers ||= {}
     end
   end
 end
