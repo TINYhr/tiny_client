71bd66d3bbe9d1d431c7571ec47a5d9cfcecf891
Phong Nguyen <phong@tinypulse.com>
2025-02-10 22:26:11 +0700

    [OOO-2706] Support Rails 8 (#11)
    * [OOO-2706] Support Rails 8
    
    * Fix failed specs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,6 @@ require 'minitest/autorun'
 require 'webmock/minitest'
 require 'mocha/minitest'
 require 'byebug'
+require "logger"
+require "active_support/core_ext/array/extract_options"
 require 'tiny_client'
